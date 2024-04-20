f2a8b36cab382f72edcf378d910861a1c38cb5d3
Daniel Ahmed <115207450+danielahmeed@users.noreply.github.com>
2024-04-20 11:41:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+// scrollActions.js
+
+var b2Icon = document.getElementById("b2Icon");
+if (b2Icon) {
+  b2Icon.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangleImage6']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b1Icon = document.getElementById("b1Icon");
+if (b1Icon) {
+  b1Icon.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangleImage5']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b2Icon1 = document.getElementById("b2Icon1");
+if (b2Icon1) {
+  b2Icon1.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangleImage4']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b2Icon2 = document.getElementById("b2Icon2");
+if (b2Icon2) {
+  b2Icon2.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangleImage3']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b2Icon3 = document.getElementById("b2Icon3");
+if (b2Icon3) {
+  b2Icon3.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangleImage2']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b1Icon1 = document.getElementById("b1Icon1");
+if (b1Icon1) {
+  b1Icon1.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangle2']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b1Icon2 = document.getElementById("b1Icon2");
+if (b1Icon2) {
+  b1Icon2.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangle1']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b1Icon3 = document.getElementById("b1Icon3");
+if (b1Icon3) {
+  b1Icon3.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangle']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b2Icon4 = document.getElementById("b2Icon4");
+if (b2Icon4) {
+  b2Icon4.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangleImage1']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var b1Icon4 = document.getElementById("b1Icon4");
+if (b1Icon4) {
+  b1Icon4.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='rectangleImage']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
